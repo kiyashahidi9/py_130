a287969bcb162025fb7568504cb862b89745d4bc
Kiya Shahidi <kiyashahidi9@gmail.com>
2025-02-27 21:03:54 +0000

    final version of todo_starter

--- todo_starter/app.py
+++ todo_starter/app.py
@@ -1,5 +1,6 @@
 from uuid import uuid4
 from functools import wraps
+import os
 
 from flask import (Flask, 
                    render_template, 
@@ -179,4 +180,7 @@ def edit_list_name(lst, list_id):
     return redirect(url_for('show_list', list_id=list_id))
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5003)
+    if os.environ.get('FLASK_ENV') == 'production':
+        app.run(debug=False)
+    else:
+        app.run(debug=True, port=5003)
